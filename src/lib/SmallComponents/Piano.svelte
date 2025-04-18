7126abc0af141d3c1b13065ae3d5c4c531a08c62
Bryson LeBlanc <bwl0016@auburn.edu>
2025-04-18 08:21:48 -0500

    piano with hover effect

--- src/lib/SmallComponents/Piano.svelte
+++ src/lib/SmallComponents/Piano.svelte
@@ -1,1 +1,33 @@
-<h4>PIANO</h4>
+<script lang="ts">
+    import Octave from '$lib/SmallComponents/Octave.svelte';
+</script>
+
+<div id="piano">
+    <div class="octave 1">
+        <Octave />
+    </div>
+    <div class="octave 2">
+        <Octave />
+    </div>
+    <div class="octave 3">
+        <Octave />
+    </div>
+</div>
+
+<style>
+    .octave {
+        display: flex;
+        flex-direction: row;
+        justify-content: center;
+        align-items: center;
+    }
+    #piano {
+        display: flex;
+        flex-direction: row;
+        justify-content: center;
+        align-items: center;
+        width: 100%;
+        height: 100%;
+        margin-bottom : 10px;
+    }
+</style>
