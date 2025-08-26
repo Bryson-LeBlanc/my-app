bdf1fe93ba1ad37036a9b2c45156b2c4933f0a1f
Bryson LeBlanc <bwl0016@auburn.edu>
2025-08-26 11:56:28 -0500

    remove deploy workflow and reset config

--- svelte.config.js
+++ svelte.config.js
@@ -1,23 +1,16 @@
-import adapter from '@sveltejs/adapter-static';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import adapter from '@sveltejs/adapter-auto';
 
 /** @type {import('@sveltejs/kit').Config} */
 
-const dev = process.argv.includes('dev');
 
 const config = {
-	preprocess: vitePreprocess(),
+
 	kit: {
 		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter({
-			fallback: 'index.html'
-		}),
-		paths: {
-			base: dev ? '' : '/my-app',
-			// assets: dev ? '' : '/my-app'
-		}
+		adapter: adapter()
+
 	}
 };
 
